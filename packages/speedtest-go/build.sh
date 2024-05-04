899e9522a8d671ecde1c41d644f5ad7a40e1e446
zongou <zongou@outlook.com>
2024-05-04 16:44:21 +0800

    Added: speedtest-go 1.7.0

---
+++ packages/speedtest-go/build.sh
@@ -0,0 +1,14 @@
+PKG_HOMEPAGE=https://github.com/showwin/speedtest-go/
+PKG_DESCRIPTION="Command line interface to test internet speed using speedtest.net"
+PKG_LICENSE="MIT"
+
+PKG_VERSION="1.7.0"
+PKG_BASENAME=speedtest-go-${PKG_VERSION}
+PKG_EXTNAME=.tar.gz
+PKG_SRCURL=https://github.com/showwin/speedtest-go/archive/refs/tags/v${PKG_VERSION}.tar.gz
+
+build() {
+	setup_go_toolchain
+	go build -ldflags='-w -s'
+	install -Dt "${OUTPUT_DIR}/bin" speedtest-go
+}
