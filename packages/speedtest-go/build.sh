68840c5603302d2c493baf909a69c48c660753d7
zongou <zongou@outlook.com>
2024-05-16 06:28:01 +0800

    f

--- packages/speedtest-go/build.sh
+++ packages/speedtest-go/build.sh
@@ -2,7 +2,7 @@ PKG_HOMEPAGE=https://github.com/showwin/speedtest-go/
 PKG_DESCRIPTION="Command line interface to test internet speed using speedtest.net"
 PKG_LICENSE="MIT"
 
-PKG_VERSION="1.7.5"
+PKG_VERSION="1.7.6"
 PKG_BASENAME=speedtest-go-${PKG_VERSION}
 PKG_EXTNAME=.tar.gz
 PKG_SRCURL=https://github.com/showwin/speedtest-go/archive/refs/tags/v${PKG_VERSION}.tar.gz
