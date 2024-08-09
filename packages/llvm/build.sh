aff6f4a150f516205618a6fe4579b9d1c886d4c9
zongou <zongou@outlook.com>
2024-08-09 06:14:12 +0000

    f

--- packages/llvm/build.sh
+++ packages/llvm/build.sh
@@ -7,47 +7,48 @@ PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_
 
 PKG_BASENAME=llvm-project-${PKG_VERSION}.src
 
-configure() {
-	# ## Small build, stripped
-	# export CFLAGS="-Os -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables"
-	# export LDFLAGS="-Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
+# configure() {
+# 	# ## Small build, stripped
+# 	# export CFLAGS="-Os -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables"
+# 	# export LDFLAGS="-Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
 
-	# export LDFLAGS="-w -s"
-	# ./configure \
-	# 	--enable-static \
-	# 	--disable-shared \
-	# 	--enable-sandbox=no \
-	# 	--prefix="${OUTPUT_DIR}" \
-	# 	--host="${TARGET}"
+# 	# export LDFLAGS="-w -s"
+# 	# ./configure \
+# 	# 	--enable-static \
+# 	# 	--disable-shared \
+# 	# 	--enable-sandbox=no \
+# 	# 	--prefix="${OUTPUT_DIR}" \
+# 	# 	--host="${TARGET}"
 
-	# sudo apt install ninja-build binfmt-support qemu-user-static
+# 	# sudo apt install ninja-build binfmt-support qemu-user-static
 
-<<<<<<< HEAD
-	export CC="${ROOT_DIR}/wrappers/zig/bin/cc --target=${TARGET}"
-	export CC="${ROOT_DIR}/wrappers/zig/bin/c++ --target=${TARGET}"
-=======
-	export CC="${ROOTDIR}/wrappers/zig/bin/cc --target=${TARGET}"
-	export CC="${ROOTDIR}/wrappers/zig/bin/c++ --target=${TARGET}"
->>>>>>> fc032b7 (update)
-	export LDFLAGS="-s"
+# 	export CC="${ROOT_DIR}/wrappers/zig/bin/cc --target=${TARGET}"
+# 	export CC="${ROOT_DIR}/wrappers/zig/bin/c++ --target=${TARGET}"
+# 	export LDFLAGS="-s"
 
-	mkdir -p build && cd build || exit
+# 	mkdir -p build && cd build || exit
 
-	cmake ../llvm \
-		-G Ninja \
-		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_INSTALL_PREFIX="${PWD}/output" \
-		-DLLVM_ENABLE_PROJECTS="clang-tools-extra" \
-		-DLLVM_TARGETS_TO_BUILD="all" \
-		-DLLVM_HOST_TRIPLE="${TARGET}" \
-		-DCMAKE_SKIP_INSTALL_RPATH=TRUE
+# 	cmake ../llvm \
+# 		-G Ninja \
+# 		-DCMAKE_BUILD_TYPE=Release \
+# 		-DCMAKE_INSTALL_PREFIX="${PWD}/output" \
+# 		-DLLVM_ENABLE_PROJECTS="clang-tools-extra" \
+# 		-DLLVM_TARGETS_TO_BUILD="all" \
+# 		-DLLVM_HOST_TRIPLE="${TARGET}" \
+# 		-DCMAKE_SKIP_INSTALL_RPATH=TRUE
 
-}
-
-build() {
-	cmake --build . --target install
-}
-
-# check() {
-# 	test -f "${OUTPUT_DIR}/lib/liblzma.a"
 # }
+
+# build() {
+# 	cmake --build . --target install
+# }
+
+# # check() {
+# # 	test -f "${OUTPUT_DIR}/lib/liblzma.a"
+# # }
+
+PKG_DEPENDS="zlib zstd"
+
+configure() {
+	ls
+}
