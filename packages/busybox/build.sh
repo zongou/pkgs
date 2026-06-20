098fda7ab1e3e76d334904d8aa7846302793f501
zongou <zongou@outlook.com>
2026-06-20 17:36:46 +0800

    f

--- packages/busybox/build.sh
+++ packages/busybox/build.sh
@@ -11,21 +11,22 @@ configure() {
 	# cp "${PKG_CONFIG_DIR}/minimal.config" .config
 	cp "${PKG_CONFIG_DIR}/optmized.config" .config
 
-	## shell: fix SIGWINCH and SIGCHLD (in hush) interrupting line input, fixed in v1.37
-	## https://github.com/mirror/busybox/commit/93e0898c663a533082b5f3c2e7dcce93ec47076d
-	# patch -up1 <"${PKG_CONFIG_DIR}/fix_line_interrupting.patch"
-
 	patch -up1 <"${PKG_CONFIG_DIR}/0000-use-clang.patch"
 	patch -up1 <"${PKG_CONFIG_DIR}/0001-clang-fix.patch"
 	# patch -up1 <"${PKG_CONFIG_DIR}/0002-hardcoded-paths-fix.patch"
 	patch -up1 <"${PKG_CONFIG_DIR}/0003-strchrnul-fix.patch"
-	patch -up1 <"${PKG_CONFIG_DIR}/0005-no-change-identity.patch"
-	patch -up1 <"${PKG_CONFIG_DIR}/0006-miscutils-crond.patch"
-	patch -up1 <"${PKG_CONFIG_DIR}/0007-miscutils-crontab.patch"
-	patch -up1 <"${PKG_CONFIG_DIR}/0008-networking-ftpd-no-chroot.patch"
-	patch -up1 <"${PKG_CONFIG_DIR}/0009-networking-httpd-default-port.patch"
-	patch -up1 <"${PKG_CONFIG_DIR}/0011-networking-tftp-no-chroot.patch"
-	patch -up1 <"${PKG_CONFIG_DIR}/0012-util-linux-mount-no-addmntent.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0004-no-change-identity.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0005-miscutils-crond.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0006-miscutils-crontab.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0007-networking-ftpd-no-chroot.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0008-networking-httpd-default-port.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0009-networking-tftp-no-chroot.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0010-util-linux-mount-no-addmntent.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0011-busybox-1.36.1-kernel-6.8.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0012-fix-segfault.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0013-fix-ipv6.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0014-fix-ipv6-2.patch"
+	patch -up1 <"${PKG_CONFIG_DIR}/0015-selinux.patch"
 	patch -up1 <"${PKG_CONFIG_DIR}/0016-explicit_bzero.patch"
 }
 
